d0e86aec5a3b1fd679bbc21589ac914e7b10dd7d
tayjanks <taylorjenkins801@gmail.com>
2022-10-13 20:50:57 -0600

    update done

--- update.sql
+++ update.sql
@@ -2,3 +2,23 @@
 UPDATE customer
 SET fax = Null
 WHERE fax = '';
+
+--Problem 2
+UPDATE customer
+SET company = 'Self'
+WHERE company IS NULL;
+
+--Problem 3
+UPDATE customer
+SET last_name = 'Thompson'
+WHERE last_name = 'Barnett'
+
+--Problem 4
+UPDATE customer
+SET support_rep_id = 4
+WHERE email  = 'luisrojas@yahoo.cl'
+
+--Problem 5
+UPDATE track
+SET composer = 'The darkness around us'
+WHERE genre_id = 3 AND composer IS NULL;
